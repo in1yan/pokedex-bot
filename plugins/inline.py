ffac1b0e56037d5bbfd0e53e655a11b483dab0de
Andrea Ciantelli <andrea.ciantelli@gmail.com>
2021-07-28 10:36:21 +0200

    code refactoring

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,13 +1,11 @@
-from pyrogram import client, filters
-from pyrogram.types import (
-    InlineQueryResultPhoto,
-    InputMediaPhoto,
-)
+from pyrogram import client
+from pyrogram.types import InlineQueryResultPhoto
 import pypokedex
+
+from .utils import get_caption
 
 
 def answer(client, inline_query):
-    num = list(range(1, 901))
     p = pypokedex.get(dex=int(inline_query["query"]))
 
     inline_query.answer(
@@ -15,21 +13,10 @@ def answer(client, inline_query):
             InlineQueryResultPhoto(
                 title=p.name,
                 photo_url=p.sprites.front.get("shiny"),
-                caption="Name : {} \n".format(p.name)
-                + "index number : {}\n".format(p.dex)
-                + "Height : {} \n".format(p.height)
-                + "Weight : {} \n".format(p.weight)
-                + "Type : {}\n".format(*p.types, sep="',")
-                + "Base stats : \n "
-                + "\t hp : {}\n".format(p.base_stats.hp)
-                + "\t attack : {} \n".format(p.base_stats.attack)
-                + "\t defence : {} \n".format(p.base_stats.defense)
-                + "\t speed : {} \n".format(
-                    p.base_stats.speed,
-                    description="info about pokemon",
-                    thumb_url=p.sprites.front.get("shiny"),
+                caption=get_caption(p),
+                description="info about pokemon",
+                thumb_url=p.sprites.front.get("shiny"),
                 ),
-            ),
         ],
         cache_time=1,
     )
